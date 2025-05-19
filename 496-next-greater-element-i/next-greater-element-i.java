ff0127ef744cdbe983dfa357e03e6208881fe494
JAIPHIRANESH <jaiphiranesh8072@gmail.com>
2025-05-19 11:33:50 +0530

    Time: 6 ms (9.33%) | Memory: 43.5 MB (76.59%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,29 +1,30 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int[] result = new int[nums1.length];
+    Stack<Integer> st = new Stack<>();
+    int n2 = nums2.length;
+    int[] nge = new int[n2];
 
-
-        a:for(int i=0;i<nums1.length;i++){
-            int temp  = nums1[i];
-            for(int j=0;j<nums2.length;j++){
-                if(nums2[j]!=temp){
-                    continue;
-                }
-                else{
-                    for(int k=j;k<nums2.length;k++){
-                    if(nums2[k]>temp){
-                    result[i]=nums2[k];
-                    continue a;
-                    
-                }
-                }
-                 result[i]=-1;
-                }
-            
-                
-            }
-           
+    for(int i=n2-1;i>=0;i--){
+        while(!st.isEmpty() && st.peek()<=nums2[i]){
+            st.pop();
         }
-        return result;
+        if(st.isEmpty()){
+            nge[i]=-1;
+        }
+        else{
+            nge[i]=st.peek();
+        }
+        st.push(nums2[i]);
+    }
+    int[] ans = new int[nums1.length];
+        for(int i=0;i<nums1.length;i++){
+            int temp = nums1[i];
+                for(int j=0;j<nums2.length;j++){
+                    if(temp==nums2[j]){
+                        ans[i]=nge[j];
+                    }
+                }          
+        }
+        return ans;
     }
 }
