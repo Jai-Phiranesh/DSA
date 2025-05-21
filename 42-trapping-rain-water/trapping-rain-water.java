272732ee89bd8abad4cb72cc0d999f4b6e9d7f50
JAIPHIRANESH <jaiphiranesh8072@gmail.com>
2025-05-21 14:05:25 +0530

    Time: 1 ms (62.92%) | Memory: 46.1 MB (73.96%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int trap(int[] arr) {
+        int total = 0 ;
+        int n = arr.length;
+        int prefix[] = new int[n];
+        int suffix[] = new int[n];
+        prefix[0]=arr[0];
+        suffix[n-1]= arr[n-1];
+        for (int i = n - 2; i >= 0; i--) {
+            suffix[i] = Math.max(suffix[i + 1], arr[i]);
+        }
+
+        for (int i = 1; i < n; i++) {
+            prefix[i] = Math.max(prefix[i - 1], arr[i]);
+        }
+
+        for(int i=0;i<arr.length;i++){
+            total+=Math.min(prefix[i],suffix[i])-arr[i];
+        }
+        
+        return total;
+    }
+}
