1b3b7cb28116168250e83227a57644c9fdddc6b6
JAIPHIRANESH <jaiphiranesh8072@gmail.com>
2025-05-27 12:53:04 +0530

    Time: 36 ms (5.08%) | Memory: 45.6 MB (6.54%) - LeetSync

---
+++ 3540-hash-divided-string/hash-divided-string.java
@@ -0,0 +1,36 @@
+class Solution {
+    public String stringHash(String s, int k) {
+
+        int n = s.length();
+        int sub = n/k;
+        String result = "";
+        int startindex=0;
+        int endindex=k;
+
+        for(int i = 0; i<sub; i++){
+           String temp = s.substring(startindex,endindex);
+           System.out.println(temp);
+           startindex+=k;
+           endindex+=k;
+           
+
+           int sum = 0;
+           for(int j=0; j<temp.length(); j++){
+                sum+=(temp.charAt(j)-'a');
+           }
+            
+
+            sum=sum%26;
+           char res = 'a'; 
+           for(int j=0;j<sum;j++){
+            res++;
+           }
+
+           result+=res;
+        }
+
+        return result;
+
+
+    }
+}
