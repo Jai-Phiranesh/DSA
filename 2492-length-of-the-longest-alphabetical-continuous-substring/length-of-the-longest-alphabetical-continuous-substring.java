2ea3f6d111295243e9293d87bf3183f4cc445b8d
JAIPHIRANESH <jaiphiranesh8072@gmail.com>
2025-05-27 16:41:13 +0530

    Time: 11 ms (78.75%) | Memory: 45.1 MB (94.69%) - LeetSync

--- 2492-length-of-the-longest-alphabetical-continuous-substring/length-of-the-longest-alphabetical-continuous-substring.java
+++ 2492-length-of-the-longest-alphabetical-continuous-substring/length-of-the-longest-alphabetical-continuous-substring.java
@@ -3,8 +3,8 @@ class Solution {
 
         int max = 1;
         int temp = 1;
-        for(int i = 1; i<s.length();i++){
-            if(s.charAt(i-1)+1==s.charAt(i)){
+        for(int i = 0; i<s.length()-1;i++){
+            if(s.charAt(i)==s.charAt(i+1)-1){
                 temp++;
                 max=Math.max(max,temp);
             }
