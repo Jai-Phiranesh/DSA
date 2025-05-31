b330816195192044e597f06e6e66b784b31e4211
JAIPHIRANESH <jaiphiranesh8072@gmail.com>
2025-05-31 21:42:49 +0530

    Time: 16 ms (81.57%) | Memory: 44.2 MB (31.47%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,13 +1,14 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         Arrays.sort(nums);
-        HashSet<ArrayList<Integer>> set = new HashSet<>();
+       List<List<Integer>> result = new ArrayList<>();
         for(int i=0;i<nums.length;i++){
+            if(i!=0 && nums[i]==nums[i-1]) continue;
             for(int j=i+1;j<nums.length;j++){
+                 if(j>i+1 && nums[j]==nums[j-1]) continue;
                     if(i==j) continue;
                     int k = j+1;
                     int l = nums.length-1;
-
                     while(k<l){
                         long sum = nums[i];
                         sum+=nums[j];
@@ -28,10 +29,11 @@ class Solution {
                             arr.add(nums[j]);
                             arr.add(nums[k]);
                             arr.add(nums[l]);
-                               Collections.sort(arr);
-                               set.add(arr);
-                               k++;
+                            result.add(arr);
+                            k++;
                                l--;
+                                while(k<l && nums[k]==nums[k-1]) k++;
+                                while(k<l && nums[l]==nums[l+1]) l--;
 
                         }
 
@@ -39,7 +41,7 @@ class Solution {
                     }
                 }
             }
-        List<List<Integer>> result = new ArrayList<>(set);
+        
         return result;
     }
 }
