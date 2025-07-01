db36e3693bb2bd0864a077d9229a0d875f6e4d03
JAIPHIRANESH <jaiphiranesh8072@gmail.com>
2025-07-01 22:18:29 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (62.28%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -6,7 +6,6 @@ class Solution {
         for(int i=0;i<26;i++){
           
             if(!(sentence.contains(String.valueOf(s)) || sentence.contains(String.valueOf(c)))){
-                System.out.println(s);
                 return false;
             }
             s++;
