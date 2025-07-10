1dea90e83490868bb545f7a5a31facbf2cbdfb6a
JAIPHIRANESH <jaiphiranesh8072@gmail.com>
2025-07-10 22:08:17 +0530

    Time: 3 ms (85.12%) | Memory: 44.1 MB (46.14%) - LeetSync

---
+++ 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int mostWordsFound(String[] sentences) {
+        int max = 0;
+
+        for(String temp:sentences){
+            String[] te = temp.split(" ");
+          
+            max=Math.max(te.length,max);
+        }
+        return max;
+    }
+}
