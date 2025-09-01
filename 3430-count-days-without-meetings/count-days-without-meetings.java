5a2456ae8cf3ff4bcc41fe58141795d205584261
JAIPHIRANESH <jaiphiranesh8072@gmail.com>
2025-09-01 22:47:28 +0530

    Time: 36 ms (85.90%) | Memory: 100.7 MB (30.64%) - LeetSync

---
+++ 3430-count-days-without-meetings/count-days-without-meetings.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int countDays(int days, int[][] meetings) {
+
+        
+
+        int count = 0;
+
+        Comparator<int[]> com  = new Comparator<int[]>(){
+
+            public int compare(int a[],int b[]){
+                return a[0]-b[0];
+            }
+        };
+
+        Arrays.sort(meetings,com);
+        int start = meetings[0][0];
+        int end = meetings[0][1];
+
+        for(int i=1;i<meetings.length;i++){
+           if(meetings[i][0]<=end){
+                end = Math.max(end,meetings[i][1]);
+           }
+           else{
+                count += (end-start)+1;
+                start = meetings[i][0];
+                end = meetings[i][1];
+           }
+            
+        }
+
+         count += (end-start)+1;
+        
+        return days-count;
+    }
+}
