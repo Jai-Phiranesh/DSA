8f7bd42f060d073a0b17671b8c0d0330011dc94f
JAIPHIRANESH <jaiphiranesh8072@gmail.com>
2026-03-28 22:33:52 +0530

    Time: 28 ms (96.42%) | Memory: 60.3 MB (13.34%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -6,7 +6,8 @@ class Solution {
 
         for(int i=0;i<nums.length;i++){
 
-            if(i!=0 && nums[i]==nums[i-1]) continue;
+            if(i!=0 && nums[i]==nums[i-1]) 
+            continue;
 
             int j=i+1;
             int k = nums.length-1;
