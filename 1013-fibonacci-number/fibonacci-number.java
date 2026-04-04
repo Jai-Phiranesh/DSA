1c0ad3253104e4769f12f2cf0c5f524ca6b285d1
JAIPHIRANESH <jaiphiranesh8072@gmail.com>
2026-04-04 20:50:25 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (23.13%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,9 +1,23 @@
 class Solution {
 
+     public int fibrec(int n, int[] dp) {
+        
+        if (n <= 1) return n;
+
+       
+        if (dp[n] != -1) return dp[n];
+
+       
+        dp[n] = fibrec(n - 1, dp) + fibrec(n - 2, dp);
+        return dp[n];
+    }
+
     int fib(int n) {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
         if(n<=1){
             return n;
         }
-        return fib(n-1)+fib(n-2);
+        return fibrec(n,dp);
     }
 }
