1db19d4e145e395cb71641b20da6e903efb089b0
JAIPHIRANESH <jaiphiranesh8072@gmail.com>
2026-05-24 16:40:11 +0530

    Time: 1 ms (100.00%) | Memory: 43.9 MB (24.06%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -2,9 +2,11 @@ class Solution {
     public int myAtoi(String s) {
         s = s.trim();
         if (s.length() == 0) return 0;
+        
 
-        int i = 0;
+        
         int sign = 1;
+        int i = 0;
         if (s.charAt(i) == '+' || s.charAt(i) == '-') {
             sign = (s.charAt(i) == '-') ? -1 : 1;
             i++;
